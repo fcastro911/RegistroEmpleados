a4a25aae3c95896b5c3d648b9d39f623f7b2db6a
FelipeCastro <felipe.castro96@inacapmail.cl>
2024-11-27 19:05:44 -0300

    Agregar archivos de proyecto.

---
+++ RegistroEmpleados.Modelos/Modelos/Cargo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroEmpleados.Modelos.Modelos
+{
+    public class Cargo
+    {
+        public string? Nombre { get; set; } //propiedad es un campo. El signo de interrogacion es un aviso de que recibe valor nulo
+    }
+}
